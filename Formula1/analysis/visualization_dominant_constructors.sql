4fbae65f80f0f3501483d17a14beb2a6af15c3dc
sonya-stefanova <sonya-stefanova>
2024-01-07 13:34:14 +0000

    dashboards on drivers and teams reports done

--- Formula1/analysis/visualization_dominant_constructors.sql
+++ Formula1/analysis/visualization_dominant_constructors.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- MAGIC %python
--- MAGIC html_h1 = """<h1 style="color:Blue;text-align:center;font-family:Georgia;text-transform:uppercase">Report on Formula 1 Dominant Drivers</h1>"""
+-- MAGIC html_h1 = """<h1 style="color:Red;font-family:Arial;text-align:center">Report On Dominant Teams</h1>"""
 -- MAGIC displayHTML(html_h1)
 
 -- COMMAND ----------
@@ -9,51 +9,55 @@ USE formula1_presentation;
 
 -- COMMAND ----------
 
-CREATE OR REPLACE TEMP VIEW v_dominant_drivers
+CREATE OR REPLACE TEMP VIEW v_dominant_teams
 AS
-SELECT driver_name,
+SELECT team_name,
       COUNT(1) AS total_races,
       sum(calculated_points) AS total_points,
       AVG(calculated_points) AS avg_points,
-      RANK() OVER(ORDER BY AVG(calculated_points) DESC) driver_rank
+      RANK() OVER(ORDER BY AVG(calculated_points) DESC) team_rank
 FROM formula1_presentation.calculated_race_results
-GROUP BY driver_name
-HAVING count(1)>=50
+GROUP BY team_name
+HAVING count(1)>=100
 ORDER BY avg_points DESC;
 
 
 -- COMMAND ----------
 
+select * from v_dominant_teams;
+
+-- COMMAND ----------
+
 SELECT race_year,
-      driver_name,
+      team_name,
       COUNT(1) AS total_races,
       sum(calculated_points) AS total_points,
       AVG(calculated_points) AS avg_points
 FROM formula1_presentation.calculated_race_results
-WHERE driver_name in (SELECT driver_name FROM v_dominant_drivers WHERE driver_rank<=10)
-GROUP BY race_year, driver_name
+WHERE team_name in (SELECT team_name FROM v_dominant_teams WHERE team_rank<=10)
+GROUP BY race_year, team_name
 ORDER BY race_year, avg_points DESC; 
 
 -- COMMAND ----------
 
 SELECT race_year,
-      driver_name,
+      team_name,
       COUNT(1) AS total_races,
       sum(calculated_points) AS total_points,
       AVG(calculated_points) AS avg_points
 FROM formula1_presentation.calculated_race_results
-WHERE driver_name in (SELECT driver_name FROM v_dominant_drivers WHERE driver_rank<=10)
-GROUP BY race_year, driver_name
+WHERE team_name in (SELECT team_name FROM v_dominant_teams WHERE team_rank<=5)
+GROUP BY race_year, team_name
 ORDER BY race_year, avg_points DESC;
 
 -- COMMAND ----------
 
 SELECT race_year,
-      driver_name,
+      team_name,
       COUNT(1) AS total_races,
       sum(calculated_points) AS total_points,
       AVG(calculated_points) AS avg_points
 FROM formula1_presentation.calculated_race_results
-WHERE driver_name in (SELECT driver_name FROM v_dominant_drivers WHERE driver_rank<=10)
-GROUP BY race_year, driver_name
+WHERE team_name in (SELECT team_name FROM v_dominant_teams WHERE team_rank<=10)
+GROUP BY race_year, team_name
 ORDER BY race_year, avg_points DESC;
